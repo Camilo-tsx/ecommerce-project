692b727ec0a8eaeb0e225ac10ca5362dff12c268
Camilo <camiloeva1236@gmail.com>
2025-02-10 22:00:09 -0300

    arreglos para mobile de la animacion

--- src/components/HomeAnimation/HomeAnimation.tsx
+++ src/components/HomeAnimation/HomeAnimation.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import logo from "../../assets/images/logo.png";
 import "./HomeAnimation.css";
 
 export const HomeAnimation = () => {
+  const [displayNone, setDisplayNone] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDisplayNone(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (displayNone) return null;
   return (
-    <div className="header-transition">
+    <div className={`header-transition ${displayNone ? "d-none" : ""}`}>
       <div>
         <img className="logo-transition" src={logo} alt="Logo" />
       </div>
